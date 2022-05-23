4ab760f878c25e800c47346de1786047583c971d
Joseph <jsltran@gmail.com>
2022-05-23 13:28:01 -0700

    two child state

--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,24 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  // creating a variable to call the function easier 
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  //initial value for color state
+  const [color, setColor] = useState(randomColor); 
+  const [childrenColor, setChildrenColor] = useState("#FFF"); 
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    //update color with a new value
+    setColor(newRandomColor);  
+    setChildrenColor(newChildColor);
+
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
